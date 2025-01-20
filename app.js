0e1ce21819ee4c615b786d737107482337250aac
DevGabriel0402 <gabriel.profissional01@gmail.com>
2025-01-20 16:21:59 -0300

    Subindo arquivos para o git

---
+++ app.js
@@ -0,0 +1,35 @@
+const btnGerar = document.getElementById("btnGerar");
+const btnLimpar = document.getElementById("btnLimpar");
+const inputUrl = document.getElementById("inputUrl");
+const toast = document.getElementById("toast");
+const qrCodeContainer = document.getElementById("qrcode");
+qrcode = new QRCode(qrCodeContainer);
+
+btnLimpar.onclick = () => {
+    inputUrl.value = "";
+    qrCodeContainer.classList.remove("active");
+    qrCodeContainer.style.animation = "qrAnimateOut .5s forwards";
+    inputUrl.focus()
+
+    setTimeout(() => {
+        qrCodeContainer.innerHTML = "";
+        qrcode = new QRCode(qrCodeContainer);
+    }, 900);
+}
+
+btnGerar.onclick = async () => {
+    if (inputUrl.value === "") {
+        toast.classList.add("toast-active");
+        setTimeout(() => {
+            inputUrl.focus();
+            toast.classList.remove("toast-active")
+            toast.style.animation = "fadeOut .5s forwards";
+
+        }, 2000)
+
+    } else {
+        await qrcode.makeCode(inputUrl.value);
+        qrCodeContainer.classList.add("active");
+    }
+}
+
